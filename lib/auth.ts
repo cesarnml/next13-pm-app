ce199f07a68274756ce54ca6a54202b5aa6932c5
Cesar Mejia <cmejia@gmail.com>
2023-01-07 02:53:08 +0700

    limited scope of auth lib; created hashPassword

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,20 +4,9 @@ const saltRounds = 10
 
 const plainTextPassword1 = 'DFGh5546*%^__90'
 
-async function main() {
-  try {
-    const salt = await bcrypt.genSalt(saltRounds)
-    const hash = await bcrypt.hash(plainTextPassword1, salt)
-    console.log('salt:', salt)
-  } catch (error) {
-    if (error instanceof Error) {
-      return console.error(error.message)
-    }
-    //? log generic error
-    return console.error(error)
-  }
+export async function hashPassword() {
+  const salt = await bcrypt.genSalt(saltRounds)
+  return await bcrypt.hash(plainTextPassword1, salt)
 }
-
-main()
 
 // ref: https://auth0.com/blog/hashing-in-action-understanding-bcrypt/

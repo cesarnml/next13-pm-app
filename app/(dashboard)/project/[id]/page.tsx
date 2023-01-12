13494114c4690834e1f18ce79f2f93fa1f5311c0
Cesar Mejia <cmejia@gmail.com>
2023-01-12 15:23:40 +0700

    base dashboard loading and page implementation

--- app/(dashboard)/project/[id]/page.tsx
+++ app/(dashboard)/project/[id]/page.tsx
@@ -1,4 +1,32 @@
-const ProjectDetails = () => {
-  return <div>ProjectDetails</div>
+import TaskCard from '@components/TaskCard'
+import { getUserFromCookie } from '@lib/auth'
+import { prisma } from '@lib/db'
+import { ReadonlyRequestCookies } from 'next/dist/server/app-render'
+import { cookies } from 'next/headers'
+
+const getData = async (id: string) => {
+  const user = await getUserFromCookie(cookies() as ReadonlyRequestCookies)
+  if (user) {
+    const project = await prisma.project.findFirst({
+      where: { id, ownerId: user.id },
+      include: {
+        tasks: true,
+      },
+    })
+
+    return project
+  }
 }
-export default ProjectDetails
+
+export default async function ProjectPage({ params }: { params: { id: string } }) {
+  const project = await getData(params.id)
+
+  if (!project) return <div>{null}</div>
+
+  return (
+    <div className='h-full overflow-y-auto pr-6 w-1/1'>
+      {/* @ts-expect-error Server Component */}
+      <TaskCard tasks={project.tasks} title={project.name} />
+    </div>
+  )
+}
